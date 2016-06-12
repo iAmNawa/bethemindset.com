b955bd9277741c3dfe94351004ea6e83a28825b1
Paul Borawski <paulborawski14@gmail.com>
2016-06-11 17:23:48 -0700

    full working

--- bethemindset.js
+++ bethemindset.js
@@ -1,10 +1,9 @@
-var port = 99999;
+var port = 9995;
 var app = require('express')();
 
 app.use(require('compression')());
 app.use(require('body-parser').json());
 app.use(require('body-parser').urlencoded({ extended:true }));
-app.use(require('stylus').middleware(require('path').join(__dirname, 'public')));
 app.use(require('express').static('public'));
 
 require('http').createServer(app).listen(port,function(){
